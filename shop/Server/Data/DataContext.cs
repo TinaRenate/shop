da33410fb21964c4117348179c8e9712bd785ec0
tinap <tinap@LAPTOP-K7AH47EO>
2022-06-06 23:29:09 +0300

    Add project files.

---
+++ shop/Server/Data/DataContext.cs
@@ -0,0 +1,41 @@
+﻿namespace shop.Server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+                    new Product
+                    {
+                        Id = 1,
+                        Title = "rubiks",
+                        Description = "987654321",
+                        ImageUrl = "https://cattalesinc.org/wp-content/uploads/slider2/DSC_0029-2048x1360.jpeg",
+                        Price = 10.0m
+                    },
+                    new Product
+                    {
+                        Id = 2,
+                        Title = "kubiks",
+                        Description = "123456789",
+                        ImageUrl = "https://cattalesinc.org/wp-content/uploads/slider2/DSC_0029-2048x1360.jpeg",
+                        Price = 100.0m
+                    },
+                    new Product
+                    {
+                        Id = 3,
+                        Title = "rubiks kubiks",
+                        Description = "147852369",
+                        ImageUrl = "https://cattalesinc.org/wp-content/uploads/slider2/DSC_0029-2048x1360.jpeg",
+                        Price = 75.0m
+                    }
+                );
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ shop/Server/Migrations/20220606201016_ProductSeeding.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace shop.Server.Migrations
+{
+    public partial class ProductSeeding : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[] { 1, "987654321", "https://cattalesinc.org/wp-content/uploads/slider2/DSC_0029-2048x1360.jpeg", 10.0m, "rubiks" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[] { 2, "123456789", "https://cattalesinc.org/wp-content/uploads/slider2/DSC_0029-2048x1360.jpeg", 100.0m, "kubiks" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[] { 3, "147852369", "https://cattalesinc.org/wp-content/uploads/slider2/DSC_0029-2048x1360.jpeg", 75.0m, "rubiks kubiks" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
